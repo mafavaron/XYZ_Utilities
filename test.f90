bcc86691f44cceb2d4db7b7b9484137451e21492
Patrizia M. Favaron <mafavaron@mac.com>
2020-02-10 14:07:41 +0100

    Update test.f90
    Step added to test 1, to write to a file

--- test.f90
+++ test.f90
@@ -8,13 +8,17 @@ program test
     ! Locals
     character(len=256)  :: sInputFile
     type(grid_space)    :: tGrid1
+    character(len=256)  :: sOutputFile
     type(grid_space)    :: tGrid2
     integer             :: iRetCode
     
-    ! Test 1: read file
+    ! Test 1: read file, then write it with a different name. Should give the same map under Surfer
     sInputFile = ".\\out.grd"
     iRetCode = tGrid1 % fileRead(10, sInputFile)
-    print *, "Test 1: Ret code = ", iRetCode
+    print *, "Test 1, step 1: Ret code = ", iRetCode
+    sOutputFile = ".\\out1.grd"
+    iRetCode = tGrid1 % fileWrite(10, sOutputFile)
+    print *, "Test 1, step 2: Ret code = ", iRetCode
 
 end program test
 
